f6c8eccc25bc4ac9d49d78493512f0ade4821c73
sutradhaar <sutradhaar@3d0f7df2-2699-66dc-3102-3904c0d1d5fe>
2010-04-20 11:01:35 +0000

    completed 1.0

--- trunk/websitelinkextractor/MainForm.Designer.vb
+++ trunk/websitelinkextractor/MainForm.Designer.vb
@@ -27,12 +27,15 @@ Partial Class MainForm
         Me.Label1 = New System.Windows.Forms.Label
         Me.txtUrl = New System.Windows.Forms.TextBox
         Me.cmdExtract = New System.Windows.Forms.Button
+        Me.cmdExport = New System.Windows.Forms.Button
+        Me.cmdExcel = New System.Windows.Forms.Button
+        Me.LinkLabel1 = New System.Windows.Forms.LinkLabel
         CType(Me.GridView, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'wb
         '
-        Me.wb.Location = New System.Drawing.Point(12, 357)
+        Me.wb.Location = New System.Drawing.Point(21, 298)
         Me.wb.MinimumSize = New System.Drawing.Size(20, 20)
         Me.wb.Name = "wb"
         Me.wb.Size = New System.Drawing.Size(663, 80)
@@ -46,7 +49,7 @@ Partial Class MainForm
         Me.GridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
         Me.GridView.Location = New System.Drawing.Point(12, 45)
         Me.GridView.Name = "GridView"
-        Me.GridView.Size = New System.Drawing.Size(698, 356)
+        Me.GridView.Size = New System.Drawing.Size(698, 348)
         Me.GridView.TabIndex = 1
         '
         'Label1
@@ -67,25 +70,56 @@ Partial Class MainForm
         '
         'cmdExtract
         '
-        Me.cmdExtract.Location = New System.Drawing.Point(378, 16)
+        Me.cmdExtract.Location = New System.Drawing.Point(377, 15)
         Me.cmdExtract.Name = "cmdExtract"
-        Me.cmdExtract.Size = New System.Drawing.Size(111, 23)
+        Me.cmdExtract.Size = New System.Drawing.Size(87, 23)
         Me.cmdExtract.TabIndex = 4
         Me.cmdExtract.Text = "Extract Links"
         Me.cmdExtract.UseVisualStyleBackColor = True
+        '
+        'cmdExport
+        '
+        Me.cmdExport.Location = New System.Drawing.Point(572, 15)
+        Me.cmdExport.Name = "cmdExport"
+        Me.cmdExport.Size = New System.Drawing.Size(96, 23)
+        Me.cmdExport.TabIndex = 5
+        Me.cmdExport.Text = "Export CSV"
+        Me.cmdExport.UseVisualStyleBackColor = True
+        '
+        'cmdExcel
+        '
+        Me.cmdExcel.Location = New System.Drawing.Point(470, 15)
+        Me.cmdExcel.Name = "cmdExcel"
+        Me.cmdExcel.Size = New System.Drawing.Size(96, 23)
+        Me.cmdExcel.TabIndex = 5
+        Me.cmdExcel.Text = "Export to Excel"
+        Me.cmdExcel.UseVisualStyleBackColor = True
+        '
+        'LinkLabel1
+        '
+        Me.LinkLabel1.AutoSize = True
+        Me.LinkLabel1.Location = New System.Drawing.Point(12, 397)
+        Me.LinkLabel1.Name = "LinkLabel1"
+        Me.LinkLabel1.Size = New System.Drawing.Size(111, 13)
+        Me.LinkLabel1.TabIndex = 6
+        Me.LinkLabel1.TabStop = True
+        Me.LinkLabel1.Text = "www.thetechhub.com"
         '
         'MainForm
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(722, 413)
+        Me.Controls.Add(Me.LinkLabel1)
+        Me.Controls.Add(Me.cmdExcel)
+        Me.Controls.Add(Me.cmdExport)
         Me.Controls.Add(Me.cmdExtract)
         Me.Controls.Add(Me.txtUrl)
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.GridView)
         Me.Controls.Add(Me.wb)
         Me.Name = "MainForm"
-        Me.Text = "Form1"
+        Me.Text = "Website Link Extractor"
         CType(Me.GridView, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
@@ -96,5 +130,8 @@ Partial Class MainForm
     Friend WithEvents Label1 As System.Windows.Forms.Label
     Friend WithEvents txtUrl As System.Windows.Forms.TextBox
     Friend WithEvents cmdExtract As System.Windows.Forms.Button
+    Friend WithEvents cmdExport As System.Windows.Forms.Button
+    Friend WithEvents cmdExcel As System.Windows.Forms.Button
+    Friend WithEvents LinkLabel1 As System.Windows.Forms.LinkLabel
 
 End Class
